9901d357e3b38ca8456080dcc644ddf7868004a4
juhisharma-dotcom <juhi.sharma2911@gmail.com>
2026-06-21 22:57:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
     // pdf-parse reads test files at module load time; excluding it from the
     // webpack bundle prevents a "module not found" crash in the API route.
-    serverComponentsExternalPackages: ['pdf-parse', '@react-pdf/renderer'],
+    serverComponentsExternalPackages: ['pdf-parse', '@react-pdf/renderer', '@napi-rs/canvas'],
   },
 };
 
